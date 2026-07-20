90cc8ead2fbad4c28f012c35e4ca62cb56cfaf51
9bry <9bry@users.noreply.github.com>
2026-07-20 04:48:59 -0500

    Show native AlertDialog with crash details from AndroidEnvironment handler

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -14,15 +14,37 @@ public class MainActivity : MauiAppCompatActivity
     {
         AndroidEnvironment.UnhandledExceptionRaiser += (s, e) =>
         {
+            var msg = e.Exception?.ToString() ?? "Unknown";
+
             try
             {
-                App.ShowErrorOnScreen(e.Exception);
+                var dir = Android.App.Application.Context?.GetExternalFilesDir(null);
+                if (dir != null)
+                    System.IO.File.WriteAllText(
+                        System.IO.Path.Combine(dir.AbsolutePath, "crashlog.txt"),
+                        "[Native UnhandledException]\n" + msg + "\n");
             }
             catch { }
-            finally
+
+            try
             {
-                e.Handled = true;
+                RunOnUiThread(() =>
+                {
+                    try
+                    {
+                        var builder = new AlertDialog.Builder(this);
+                        builder.SetTitle("Kraken Crash");
+                        var shown = msg.Length > 3000 ? msg.Substring(0, 3000) : msg;
+                        builder.SetMessage(shown);
+                        builder.SetPositiveButton("OK", (sender, args) => { });
+                        builder.Create().Show();
+                    }
+                    catch { }
+                });
             }
+            catch { }
+
+            e.Handled = true;
         };
 
         base.OnCreate(savedInstanceState);
